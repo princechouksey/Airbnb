a7c694c20f4971ceffbd6694ac532ba20b0aed80
Prince Chouksey <145126261+princechouksey@users.noreply.github.com>
2025-07-05 18:21:52 +0530

    Update user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -42,7 +42,12 @@ const loginUserController = async (req, res,next) => {
     try {
         const user = await User.authenticateUser(email, password);
         const token = await user.generateAuthToken()
-        res.cookie("token", token)
+        res.cookie("token", newToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "None",
+        maxAge: 24 * 60 * 60 * 1000,
+        });
         res.status(200).json({
             success: true,
             message: "User logged in successfully",
@@ -81,15 +86,17 @@ const currentUserController = async (req,res,next)=>{
     try {
         const user = req.user;
         res.status(200).json({ message: "authentication successfull", user: user });
-    } catch (error) {
-        
     }
+    catch (error) {
+  next(new CustomError(error.message, 500));
+}
+
 }
 
 const updateUserProfileController = async (req,res,next)=>{
     try {
       const   {username, email, phone, address, newPassword} = req.body;
-      const user  = User.findOne({email})
+      const user  =await  User.findOne({email})
         if(!user) {
             return res.status(400).json({message: "User not found"})
         }
@@ -97,18 +104,21 @@ const updateUserProfileController = async (req,res,next)=>{
         if(email) user.email = email;
         if(phone) user.phone = phone;
         if(address) user.address = address;
-        const newToken = null;
-
-        if(password) {
-            newToken = await user.generateAuthToken(newPassword)
-            user.password = newPassword;
-        }
-
+       let newToken = null;
+       if (newPassword) {
+       newToken = await user.generateAuthToken(newPassword);
+       user.password = newPassword;
+      }
         await user.save();
         if(!newToken) {
             return next(new CustomError("error while generating new jwt token", 400));
         }
-        res.cookie("token", newToken)
+       res.cookie("token", newToken, {
+       httpOnly: true,
+       secure: true,
+       sameSite: "None",
+       maxAge: 24 * 60 * 60 * 1000,
+});
         res.status(200).json({
             success: true,
             message: "User profile updated successfully",
@@ -138,7 +148,7 @@ const resetUserPasswordController = async (req,res,next)=>{
         const rawToken = jwt.sign({ id: user._id }, process.env.JWT_RAW_SECRET, {
             expiresIn: "10m",
           });
-          const resetLink = `http://localhost/api/user/reset-password/${rawToken}`;
+          const resetLink = `${process.env.FRONTEND_URL}/reset-password/${rawToken}`;
           const emailTemplate = resetPasswordTemplate(req.user.userName, resetLink);
           await sendMail("princechouksey179@gmail.com", "Reset password", emailTemplate);
           res.status(200).json({
